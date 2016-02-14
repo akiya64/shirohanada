72c382a61974e93d6df2223aeac46ca91ceb2e0b
Akiya <autumnsky64@gmail.com>
2016-02-14 12:51:02 +0900

    gulp.js 出力先Dir修正

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var stylus = require('gulp-stylus');
 var sourcemaps = require('gulp-sourcemaps');
 var nib = require('nib');
 
-//VCCWのテーマフォルダへ書き出す
 gulp.task('stylus', function() {
 	gulp.src('stylus/style.styl')
 		.pipe(sourcemaps.init())
@@ -11,27 +10,7 @@ gulp.task('stylus', function() {
 			use: [nib()]
 			}))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('../vccw/www/wp-content/themes/shirohanada'));
-});
-
-gulp.task('stylus-single', function() {
-	gulp.src('stylus/style.styl')
-		.pipe(sourcemaps.init())
-		.pipe(stylus({
-			use: [nib()]
-			}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('css/single'));
-});
-
-gulp.task('proto-stylus', function() {
-	gulp.src('stylus/style.styl')
-		.pipe(sourcemaps.init())
-		.pipe(stylus({
-			use: [nib()]
-			}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('css/'));
+		.pipe(gulp.dest(''));
 });
 
 gulp.task('watch', function(){
