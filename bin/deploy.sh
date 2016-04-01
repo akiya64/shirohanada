b441c0627795122cfff607f1ddefc695b9e58cf3
Akiya <autumnsky64@gmail.com>
2016-04-01 14:01:45 +0900

    #63 #64 Travis CI 用ファイル追加

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	exit
+fi
+
+git status
