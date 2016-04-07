02ce6bbf8a59faf40358dbe5cb54af568a581827
Akiya <autumnsky64@gmail.com>
2016-04-07 20:55:30 +0900

    #70 検索実行ボタンを右に寄せたいので、clearfixクラス作成

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 
        <label>
                 <input type="search" class="search-field" placeholder="Search for:" value="" name="s" />
