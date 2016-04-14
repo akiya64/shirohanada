51666159c735a08f60e2c45caa4a3e686997a8e9
Akiya <autumnsky64@gmail.com>
2016-04-14 21:18:03 +0900

    #105 @30m ヘッダーをsingle/index/pageから括り出した

--- content.php
+++ content.php
@@ -15,7 +15,7 @@
 	<article id="<?php the_ID(); ?>">
 
 		<?php if(is_single()): ?>
-			<h1 class="entry-title"><?php the_title();?></h1>
+			<h1 class="entry-title single"><?php the_title();?></h1>
 		<?php else: ?>
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
 		<?php endif; ?>
