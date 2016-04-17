0e243a97b41136b6248beabd2a9fdbbb37503d7e
Akiya <autumnsky64@gmail.com>
2016-04-17 16:09:27 +0900

    #113 @3h 0.8リリース前のスタイル微調整

--- footer.php
+++ footer.php
@@ -6,8 +6,9 @@
 ?>
 
 <footer class="<?php echo $footer_class; ?>">
-	<p>(c) <?php echo get_first_post_year(); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name') ; ?></a><br>
-	Powered by <a href="http://wordpress.org/"><i class="icon icon-wordpress"></i>WordPress</a>, Hosting by <a href="http://www.sakura.ad.jp/">さくらインターネット</a></p>
+	<p>(c) <?php echo get_first_post_year(); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name') ; ?></a></p>
+	<p>Powered by <a href="http://wordpress.org/"><i class="icon icon-wordpress"></i>WordPress</a>, </p>
+	<p>Hosting by <a href="http://www.sakura.ad.jp/">さくらインターネット</a></p>
 </footer>
 
 <?php if (is_front_page()): ?>
