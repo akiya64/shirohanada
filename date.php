07fa7a76571fa585f416f03b451c11f7ac694eeb
Akiya <autumnsky64@gmail.com>
2016-04-27 21:41:44 +0900

    #121 date.phpを日付アーカイブ用に新規作成

---
+++ date.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<?php 
+	//Current CatName or Date //
+	if( is_category() ) {
+		$current_page_title = single_cat_title("", False );
+	}else{
+		$current_page_title = 'Archive ' . get_the_time('Y') . '.' . get_the_time('m');
+	}
+?>
+
+<div class="main-contents">
+
+<div class="articles">
+
+	<h2 class="page-title">
+		<?php echo $current_page_title; ?>
+	</h2>
+
+	<?php
+		// Display posts
+		// Start the loop.:
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/content' );
+		}
+
+		get_template_part( 'template-parts/nav', 'pagenate' );
+		get_template_part( 'template-parts/nav', 'posts' );
+	?>
+
+</div><!--end articles-->
+
+<?php get_sidebar(); ?>
+
+</div><!--end main contents-->
+
+<?php get_footer(); ?>
