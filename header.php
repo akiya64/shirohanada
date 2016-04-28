0c4786b19efadc07228147853b50d6b203eca599
Akiya <autumnsky64@gmail.com>
2016-04-28 20:44:08 +0900

    Faviconはダッシュボードからサイトアイコンとして登録できるので、テーマ側で指定しない。

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
 	<title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
 
 	<?php wp_head(); ?>
