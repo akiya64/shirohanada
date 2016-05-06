bf0400c975d628dc440583216167689d1c8e1acb
Akiya <autumnsky64@gmail.com>
2016-05-06 20:45:44 +0900

    #153 変数に入れてecho部分だけ括り出すことで開始タグと終了タグの間は1行で済ませた。

--- template-parts/nav-pagenate.php
+++ template-parts/nav-pagenate.php
@@ -1,28 +1,26 @@
+<?php
+	/* Build pagination by WordPress embedded method. */
+	global $wp_query;
+	$big = 999999999; // need an unlikely integer
+	$paginate_links = paginate_links( array(
+		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'format' => '?paged=%#%',
+		'current' => max( 1, get_query_var('paged') ),
+		'mid_size' => 3,
+		'total' => $wp_query->max_num_pages,
+		'prev_next' => False
+		) );
+
+	/* Current CatName or Date */
+	if( is_category() ):
+		$page_title = single_cat_title('', False ).' ';
+	elseif( is_archive() ):
+		$page_title = 'Archive ' . the_date( 'Y.m', '','', False ) . ' ';
+	else:
+		$page_title = 'Posts ';
+	endif;
+?>
+
 <nav class="pagenation text-centering">
-	<?php
-		global $wp_query;
-		$big = 999999999; // need an unlikely integer
-		$paginate_links = paginate_links( array(
-			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-			'format' => '?paged=%#%',
-			'current' => max( 1, get_query_var('paged') ),
-			'mid_size' => 3,
-			'total' => $wp_query->max_num_pages,
-			'prev_next' => False
-			) );
-
-		/* Current CatName or Date */
-		if( is_category() ):
-			$page_title = single_cat_title('', False ).' ';
-		elseif( is_archive() ):
-			$page_title = 'Archive ' . the_date( 'Y.m', '','', False ) . ' ';
-		else:
-			$page_title = 'Posts ';
-		endif;
-
-		echo $page_title . $paginate_links;
-
-	?>
+	<?php echo $page_title . $paginate_links; ?>
 </nav>
-
-
