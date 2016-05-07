83bd2904d1afb805e63cbfd8cf4b144e8f3659d0
Akiya <autumnsky64@gmail.com>
2016-05-07 18:45:04 +0900

    sidebarの登録順を入れ替えた。テーマ有効化時にデフォルトのウィジェットがSide Bar Menuへ入って欲しいので。

--- functions.php
+++ functions.php
@@ -14,18 +14,18 @@
  */
 function shirohanada_widgets_init() {
 
-	register_sidebar( array(
-		'name' => 'Front Page Widget',
-		'id' => 'front-widgets',
+register_sidebar( array(
+		'name' => 'Side Bar Menu',
+		'id' => 'sidebar-widgets',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title' => '<h2 class="widget-title">',
 		'after_title' => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
-		'name' => 'Side Bar Menu',
-		'id' => 'sidebar-widgets',
+		'name' => 'Front Page Widget',
+		'id' => 'front-widgets',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title' => '<h2 class="widget-title">',
