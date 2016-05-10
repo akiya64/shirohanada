48e78c222ddb9a9c7e650faeaeca38e8663588ff
Akiya <autumnsky64@gmail.com>
2016-05-10 21:13:34 +0900

    #157 front-page.phpをカスタムヘッダーとした時にコメントアウトした固定IMG用のコードが残っていた。

--- front-page.php
+++ front-page.php
@@ -13,8 +13,6 @@ get_header(); ?>
 
 	<div class="top-banner-box">
 		<img src="<?php header_image(); ?>" alt="top banner" class="top-banner">
-
-		<!--img class="top-banner" src="<?php echo get_stylesheet_directory_uri(); ?>/top_banner.jpg" -->
 	</div>
 
 	<header class="top-page-header">
