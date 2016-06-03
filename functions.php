2c65353e6daddff69ebdcc245564ef976c7bf0b2
Akiya <autumnsky64@gmail.com>
2016-06-03 16:12:17 +0900

    first add

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Shirohanada functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Shirohanada
+ * @since Shirohanada 0.8
+ */
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+?>
