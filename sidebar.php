3f2cdc2de8c14042dbf70cd93dfd77fe86c95e62
Akiya <autumnsky64@gmail.com>
2016-10-16 20:42:31 +0900

    #198 1h モバイル時のレイアウト崩れを修正。

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<aside id="side-menu" class="widget-area">
+<aside id="side-menu" class="side-widgets">
 	<button id="hide-menu-button" class="button hide-menu-button"><i class="icon icon-exit"></i></button>
 
 	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
