919cc0bc2be4ab890bff1166a3cc6bb7bf64d82f
travis-ci <autumnsky64@gmail.com>
2016-11-26 11:56:51 +0000

    prepare release

--- phpcbf-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-find . template-parts/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v -n {} --standard=wordpress \;
-find . template-parts/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v -n {} --standard=wordpress \;
-find . template-parts/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v -n {} --standard=wordpress \;
-find . template-parts/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v -n {} --standard=wordpress \;
