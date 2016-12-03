39a796f411d40ce551b04cfd047b54072a76b523
Akiya <autumnsky64@gmail.com>
2016-12-03 16:57:11 +0900

    #215 @1h index.phpテンプレートにて検索時の投稿表示に対応
    404.phpをコンポーネント化して、投稿が見つからないときに表示するようにした

--- 404.php
+++ 404.php
@@ -9,43 +9,10 @@
 
 get_header(); ?>
 
-<div class="main-contents">
-
-<div class="article-area" role="main" itemscope itemtype="schema.org/BlogPosting" itemprop="articleBody">
-
 	<h2 class="page-title">
 		404 Not Found
 	</h2>
 
-<article class="page nomatch-contents">
-
-	<h3>ページが見つかりませんでした。</h3>
-	
-	<ol class="reasons">
-		<li>未来の日付へのリンクが生成されている場合があります。</li>
-		<li>記事のない日付アーカイブへのリンクだったかも知れません。</li>
-		<li>あるいは、ページが削除された可能性があります。</li>
-	</ol>
-
-	<h3>Search Posts</h3>
-		<div class="_halfwidth-inlarge">
-			<?php get_search_form(); ?>
-		</div>
-
-	<h3>Show Posts</h3>
-		<nav>
-			<p class="move-recent-posts">
-				<a href="<?php echo esc_url( get_month_link( '', '' ) ); ?>">Recent Posts</a>
-			</p>
-		</nav>
-
-</article>
-
-
-</div><!--end article-area-->
-
-<?php get_sidebar(); ?>
-
-</div><!--end main contents-->
+<?php get_template_part( 'components/notfound' ); ?>
 
 <?php get_footer(); ?>
