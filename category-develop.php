ec57fb096ff76167cf63fd7abb98d8be5750159e
Akiya <autumnsky64@gmail.com>
2017-02-08 19:32:14 +0900

    #188 現行のDevelopカテゴリーテンプレートに抜粋切り替えを実装

--- category-develop.php
+++ category-develop.php
@@ -7,50 +7,48 @@
  * @since Shirohanada 0.8
  */
 
-get_header(); ?>
-
-<?php
-	if( is_category() ) :
-		$current_page_title = '<h2 class="page-title">'.single_cat_title("", False ).'</h2>';
-	elseif ( is_tag() ) :
-		$current_page_title = '<h2 class="page-title">'.single_tag_title("", False ).'</h2>' ;
-	elseif ( is_single() ) :
-		$current_page_title = '' ;
-	else :
-		/* this case is expected only RecentPost */
-		$current_page_title =  '<h2 class="page-title">Latest Posts</h2>';
-	endif ;
+get_header();
 ?>
 
+	<h2 class="page-title"><?php echo single_cat_title('', false); ?></h2>
 
-<?php
-$tid = get_query_var( 'cat' );
-$is_show_excerpt = get_option( "cat_$tid_show_excerpt");
-echo $is_show_excerpt;
-?>
-	<?php echo $current_page_title; ?>
+        <?php
+             $tid = get_query_var( 'cat' );
+             $is_show_excerpt = get_option( "cat_$tid_show_excerpt");
 
-	<?php
-		/* Display posts Start the loop.*/
-		while ( have_posts() ):
-			the_post();
-	?>
-	<article id="<?php the_ID(); ?>" <?php post_class(); ?>>
+	if ($is_show_excerpt): ?>
+	
+		<?php
+			/* Display posts Start the loop.*/
+			while ( have_posts() ):
+				the_post();
+		?>
 
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
+		<article id="<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<header class="entry-header">
-			<p class="entry-date"><i class="icon icon-time"></i><a href="<?php echo get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d')); ?>"><time pubdate="<?php the_time('Y-n-j'); ?>"><?php the_time('Y.n.j'); ?></time></a></p>
-			<?php the_tags('<p><i class="icon icon-tag"></i>', ', ' ,'</p>'); ?>
-		</header>
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h1>
 
-		<div class="entry-content">
-			<?php the_excerpt(); ?>
-		</div><!--end entry-content-->
+			<header class="entry-header">
+				<p class="entry-date"><i class="icon icon-time"></i><a href="<?php echo get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d')); ?>"><time pubdate="<?php the_time('Y-n-j'); ?>"><?php the_time('Y.n.j'); ?></time></a></p>
+				<?php the_tags('<p><i class="icon icon-tag"></i>', ', ' ,'</p>'); ?>
+			</header>
+
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
+			</div><!--end entry-content-->
 
 		</article>
-			
+	
 		<?php endwhile; ?>
+
+	<?php else: 
+		/* Display posts Start the loop.*/
+		while ( have_posts() ) : the_post();
+			get_template_part( 'components/content' );
+		endwhile;
+	endif; ?>
+
+		
 
 	<nav class="link-posts text-centering">
 		<?php get_template_part( 'template-parts/navigation' ); ?>
