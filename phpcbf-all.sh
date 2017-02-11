4e6cd36c185e74524bd92bf7ccf4d21b626d2db0
Akiya <autumnsky64@gmail.com>
2017-02-11 14:04:52 +0900

    warningも表示するようチェックスクリプトを修正

--- phpcbf-all.sh
+++ phpcbf-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v -n {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v -n {} --standard=wordpress \;
-find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v -n {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v -n {} --standard=wordpress \;
+find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v {} --standard=wordpress \;
