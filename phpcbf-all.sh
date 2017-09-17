d6891126da499954c894230be62fe5331717beac
Akiya <autumnsky64@gmail.com>
2017-09-17 18:03:44 +0900

    #238 @1h phpcsのWordPress規約セットをバージョンアップ
    ルールセットの最新版に差し替えたところエラー発生、CodeSnifferから入れ替えた。

--- phpcbf-all.sh
+++ phpcbf-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcbf -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.php" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./vendor/bin/phpcs -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.php" -maxdepth 1 -exec ./phpcs/bin/phpcbf -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.css" -maxdepth 1 -exec ./phpcs/bin/phpcbf -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.php" -maxdepth 1 -exec ./phpcs/bin/phpcs -p -s -v {} --standard=wordpress \;
+find . components/ -name "*.css" -maxdepth 1 -exec ./phpcs/bin/phpcs -p -s -v {} --standard=wordpress \;
