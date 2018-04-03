73b4d2fc17e10241b861130381a39dc5ebe3c202
Akiya <autumnsky64@gmail.com>
2018-04-03 17:57:09 +0900

    #243 titleタグの呼び出しをWordPress4.4以降の推奨方法に変更

--- header.php
+++ header.php
@@ -14,8 +14,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
-
 	<?php wp_head(); ?>
 </head>
 
