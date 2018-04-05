2aea00961446309d49fa665e75d4909fff6fe7db
Akiya <autumnsky64@gmail.com>
2018-04-05 16:23:11 +0900

    #247 テーマでサポートする機能の指定

--- functions.php
+++ functions.php
@@ -7,20 +7,28 @@
  * @since      Shirohanada 0.8
  */
 
+/**
+ * Set content width
+ *
+ * @since Shirohanada 0.11
+ */
 if ( ! isset( $content_width ) ) {
 	$content_width = 690;
 }
 
-add_editor_style();
-add_theme_support( 'title-tag' );
-
 /**
- * Enable post thumnails.
+ * Enable theme support feature.
  *
  * @since Shirohanada 0.9.1
  */
+function custom_theme_setup() {
+	add_editor_style();
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'post-thumbnails' );
+}
 
-add_theme_support( 'post-thumbnails' );
+add_action( 'after_setup_theme', 'custom_theme_setup' );
 
 /**
  * Remove header ,not using Shirohanada
