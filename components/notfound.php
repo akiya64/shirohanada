0a99667aa17cb6930b20a7b17def5630d9f2432d
Akiya <autumnsky64@gmail.com>
2019-04-14 16:23:18 +0900

    404ページの改善

--- components/notfound.php
+++ components/notfound.php
@@ -9,34 +9,27 @@
 
 ?>
 
-<article class="page nomatch-contents">
+<article class="type-page nomatch-contents">
 
-<?php if ( is_search() ) : ?>
-<h3>記事が見つかりません</h3>
+	<?php if ( is_search() ) : ?>
+		<p class="description">検索ワード："<?php echo get_search_query( false ); ?>"を含む記事はありませんでした。<br>
+		</p>
 
-<p class="reasons">検索ワード："<?php echo get_search_query( false ); ?>"を含む記事はありませんでした。<br>
-別の単語で検索してみて下さい。</p>
+	<?php else : ?>
 
-<?php else : ?>
-	<h3>ページが見つかりませんでした</h3>
-	
-	<ol class="reasons">
-		<li>未来の日付へのリンクが生成されている場合があります。</li>
-		<li>記事のない日付アーカイブへのリンクだったかも知れません。</li>
-		<li>あるいは、ページが削除された可能性があります。</li>
-	</ol>
+		<p class="description">お探しのページが見つかりませんでした。<br>移動、削除されたかも知れません。</p>
+
 <?php endif; ?>
 
-	<h3>Search Posts</h3>
-		<div class="_halfwidth-inlarge">
-			<?php get_search_form(); ?>
-		</div>
-
-	<h3>Show Posts</h3>
-		<nav>
-			<p class="move-recent-posts">
-				<a href="<?php echo esc_url( get_month_link( '', '' ) ); ?>">Recent Posts</a>
-			</p>
+		<nav class="links _halfwidth-inlarge">
+			<a class="link" href="<?php echo esc_url( get_month_link( '', '' ) ); ?>">最新の投稿をみる</a><a class="link" href="<?php echo esc_url( home_url() ); ?>">トップページへ戻る</a>
 		</nav>
 
+		<h3>Search Posts</h3>
+			<div class="_halfwidth-inlarge">
+				<?php get_search_form(); ?>
+			</div>
+
+
+	</article>
 </article>
