2f4f16cd6f31554c1d3cfb9fe4096baf3f68058a
akiya64-in-travis <autumnsky64@gmail.com>
2020-04-14 02:14:30 +0000

    prepare release

--- phpcbf-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-find . components/ -name "*.php" -maxdepth 1 -exec ./phpcs/bin/phpcbf -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./phpcs/bin/phpcbf -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.php" -maxdepth 1 -exec ./phpcs/bin/phpcs -p -s -v {} --standard=wordpress \;
-find . components/ -name "*.css" -maxdepth 1 -exec ./phpcs/bin/phpcs -p -s -v {} --standard=wordpress \;
